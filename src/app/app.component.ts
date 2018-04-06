069b25bb34580c9cb0d1b4240590400877f2d7cc
Gaurav Singh <singhgaurav4242@gmail.com>
2018-04-06 15:05:34 +0530

    updates ui with loop ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  techStack=[
+    {
+      name: "Angular 5",
+      type:"Front End",
+      questions:"150",
+      lastupdate:"",
+      totalVistors:"",
+      resloc:"angular"
+    },
+    {
+      name: "Node JS ",
+      questions:"150",
+      type:"Back End",
+      lastupdate:"",
+      totalVistors:"",
+      resloc:"node"
+    },
+    {
+      name: "Vanilla JS",
+      questions:"130",
+      type:"Front End",
+      lastupdate:"",
+      totalVistors:"",
+      resloc:"vjs"
+    },
+    {
+      name: "Mongo DB",
+      questions:"110",
+      type:"Database",
+      lastupdate:"",
+      totalVistors:"",
+      resloc:"mongo"
+    },
+    {
+      name: "Express JS",
+      questions:"50",
+      type:"Back End",
+      lastupdate:"",
+      totalVistors:"",
+        resloc:"express"
+    },
+    {
+      name: "AWS",
+      questions:"20",
+      type:"Cloud",
+      lastupdate:"",
+      totalVistors:"",
+        resloc:"aws"
+    }
+  ]
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
-
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [BrowserModule, MatCardModule,MatButtonModule, MatGridListModule],
+  imports: [BrowserModule, MatCardModule,MatButtonModule, MatGridListModule,MatIconModule],
   providers: [],
   bootstrap: [AppComponent]
 })
